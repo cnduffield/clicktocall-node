cdef314f3899a17e2400da71b4ce76491a0dc185
cnduffield <cnduffield@hotmail.com>
2016-09-16 23:34:49 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@
 // system environment variables (so we don't check them in to source control!)
 module.exports = {
     // Twilio Account SID - found on your dashboard
-    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    accountSid: process.env.TWILIO_ACCOUNT_SID || AC16963bc3c354b65b7270d3aa9ab7eb4b,
 
     // Twilio Auth Token - found on your dashboard
-    authToken: process.env.TWILIO_AUTH_TOKEN,
+    authToken: process.env.TWILIO_AUTH_TOKEN || 53be0a3af318ffbef6d92a860dda3c29,
 
     // A Twilio number that you have purchased through the twilio.com web
     // interface or API
-    twilioNumber: process.env.TWILIO_NUMBER,
+    twilioNumber: process.env.TWILIO_NUMBER || +14155552671,
 
     // The port your web application will run on
     port: process.env.PORT || 3000
